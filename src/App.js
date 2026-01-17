78f1d24478100b198756ed4b584f5f34fa694207
suraj gupta <baniyasuraj143@gmail.com>
2026-01-18 00:05:53 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './card';
+
+function App() {
+  return (
+    <>
+    <Card name='suraj' age={18}/>
+    </>
+  );
+}
+
+export default App;
